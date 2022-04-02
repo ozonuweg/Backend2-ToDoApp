b405834ce2bc622f6b8f320a2e04e69133f66bd4
Glory Ozonuwe <ozo18001@byui.edu>
2022-04-01 20:01:30 -0600

    3

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swaggerDesign.json');
+const swaggerDocument = require('../swaggerDesign3.json');
 // const swaggerDocument = require('../swaggerdesign.json'); // don't want to override this with npm run swagger
 router.use('/api-docs', swaggerUi.serve);
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
